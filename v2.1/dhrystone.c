e2dc4dd88a24b281f7dd35a2624f2f8ad9060bf2
sfujibijutsukan <pooh.sf.1253@gmail.com>
2024-12-04 22:29:55 +0900

    update LOOPS

--- v2.1/dhrystone.c
+++ v2.1/dhrystone.c
@@ -384,9 +384,9 @@
 
 /* Accuracy of timings and human fatigue controlled by next two lines */
 /*#define LOOPS	5000		/* Use this for slow or 16 bit machines */
-#define LOOPS	50000		/* Use this for slow or 16 bit machines */
+//#define LOOPS	50000		/* Use this for slow or 16 bit machines */
 /*#define LOOPS	500000		/* Use this for faster machines */
-
+#define LOOPS	10000000		/* Use this for slow or 16 bit machines */
 /* Compiler dependent options */
 #undef	NOENUM			/* Define if compiler has no enum's */
 #undef	NOSTRUCTASSIGN		/* Define if compiler can't assign structures */

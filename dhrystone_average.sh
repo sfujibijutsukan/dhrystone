2215d4eaf2f8ce1eac7cc84ee7fd270f92d5e21a
sfujibijutsukan <pooh.sf.1253@gmail.com>
2024-12-30 16:59:09 +0900

    引数で命令数を指定してもらうように変更

--- dhrystone_average.sh
+++ dhrystone_average.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+    echo "第一引数として実行する命令数を指定してください (5000, 50000, 500000, 10000000, 100000000, 1000000000)"
+    exit 1
+fi
+
 # 結果を保存する配列
 results=()
 
 # Dhrystone を 3 回実行
 for i in {1..5}; do
     # 実行結果を取得（ここではコマンドをシミュレート）
-    output=$(v2.1/dhrystone)
+    output=$(v2.1/dhrystone_loops_$1)
     
     # 値を抽出して配列に追加
     value=$(echo "$output" | grep "benchmarks at" | awk '{print $5}')
